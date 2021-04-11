f58f8e575d38cf9bdf7716398f43833ea83f87ce
Sandro Rybárik <sandro.rybarik@gmail.com>
2021-04-11 13:24:05 +0200

    setup py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tf-semiseg-dataloader-SandroRybarik',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='Custom dataloader for semiseg segmentation dataset',
+    long_description=open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    install_requires=['tensorflow', 'numpy'],
+    url='https://github.com/SandroRybarik/tf-semiseg-dataloader',
+    author='SandroRybarik',
+    author_email='rybarik@fisherio.com'
+)
