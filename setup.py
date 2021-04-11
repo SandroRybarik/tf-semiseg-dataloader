c6ad95a65222d9a69eb2b1e4b13bde8a8dd49642
Sandro Rybárik <sandro.rybarik@gmail.com>
2021-04-11 13:59:59 +0200

    name finalized + added __init__ import

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tf-semiseg-dataloader-SandroRybarik',
+    name='tf-semiseg-dataloader',
     version='0.0.1',
     packages=find_packages(),
     license='MIT',
